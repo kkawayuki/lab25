36721f6b5fd38e7cf0e5d221d00fcbb0841aab0b
kkawayuki <kyukiokawa@gmail.com>
2024-10-27 14:39:15 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
 #include <chrono>
+#include <vector> //for vector
+#include <list> //for list
 using namespace std;
+using namespace std::chrono;
 
-int main() {
-    
-    
+//function prototypes
+auto vRead();
+auto vSort();
+auto vInsert();
+auto vDelete();
+
+auto lRead();
+auto lSort();
+auto lInsert();
+auto lDelete();
+
+auto printRecords();
+
+int main()
+{
+    //declare vector/string objects
+    vector<string>myVec;
+    list<string>myList;
+
+    printRecords();
+
+    auto start = high_resolution_clock::now();
+    auto end = high_resolution_clock::now();
     return 0;
+
+
 }
 
+//function implementations
+
+auto vRead()
+{
+
+}
+
+auto vSort()
+{}
+auto vInsert(){}
+auto vDelete(){}
+
+auto lRead(){}
+auto lSort(){}
+auto lInsert(){}
+auto lDelete(){}
+
+auto printRecords()
+{
+    //table that calls all prior functions
+}
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
