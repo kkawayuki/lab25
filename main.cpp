69fa47b27f3c06c61ea38487d65e3c389b2a2073
kkawayuki <kyukiokawa@gmail.com>
2024-10-27 14:59:17 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <vector> //for vector
 #include <list> //for list
+#include <set> //for set
+#include <algorithm> //for sort
 using namespace std;
 using namespace std::chrono;
 
@@ -11,11 +13,14 @@ auto vRead();
 auto vSort();
 auto vInsert();
 auto vDelete();
-
 auto lRead();
 auto lSort();
 auto lInsert();
 auto lDelete();
+auto sRead();
+auto sSort();
+auto sInsert();
+auto sDelete();
 
 void printRecords();
 
@@ -24,6 +29,7 @@ int main()
     //declare vector/string objects
     vector<string>myVec;
     list<string>myList;
+    set<string>mySet;
 
     printRecords();
 
@@ -39,10 +45,8 @@ auto vRead(vector<string>&myVec)
 
     ifstream fin("codes.txt");
     while(getline(fin,buf))
-    {
         myVec.push_back(buf); //add all elements to vec
-    }
-
+    
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
 
@@ -51,12 +55,12 @@ auto vRead(vector<string>&myVec)
 
 auto vSort(vector<string>&myVec)
 {
-    
+    sort(myVec.begin(),myVec.end()); //sorts entire vector
 }
 
 auto vInsert(vector<string>&myVec)
 {
-
+    myVec.insert(10000, "a");
 }
 
 auto vDelete(vector<string>&myVec)
@@ -76,16 +80,48 @@ auto lRead(list<string>&myList)
     }
 }
 
-auto lSort(list<string>&myList){return(-1)} //already sorted
+auto lSort(list<string>&myList)
+{
 
-auto lInsert(list<string>&myList){}
+}
 
-auto lDelete(list<string>&myList){}
+auto lInsert(list<string>&myList)
+{
 
+}
+
+auto lDelete(list<string>&myList)
+{
+
+}
+
+//set operations
+
+auto sRead(vector<string>&mySet);
+{
+
+}
+
+auto sSort(vector<string>&mySet) //already sorted, return -1
+{
+    return(-1);
+}  
+
+auto sInsert(vector<string>&mySet){
+
+}
+auto sDelete(vector<string>&mySet)
+{
+
+}
+
+//very important function!
 void printRecords()
 {
     //table that calls all prior functions
 }
+
+
 /* syntax examples:
 auto start = high_resolution_clock::now()
 auto end = high_resolution_clock::now()
